e43894150aa9736e3ce46d7ba1a3d6bd9d1aedbe
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-11 18:47:02 -0500

    More input fields

--- scripts/host.js
+++ scripts/host.js
@@ -23,6 +23,49 @@ function insertProfileInfo(name, picture) {
 }
 
 function submitHost() {
-	
-	console.log('hi');
+
+	var host =  new Object();
+	host.type = "host";
+	host.name = document.getElementById('name').innerHTML,
+	host.address = document.getElementById('address').value;
+	host.city = document.getElementById('city').value;
+	host.state = document.getElementById('state').value;
+	host.zip = document.getElementById('zip').value;
+	var services = [];
+
+	if (document.getElementById('water').checked) {
+		services.push('water');
+	}
+	if (document.getElementById('food').checked) {
+		services.push('food');
+	}
+	if (document.getElementById('electricity').checked) {
+		services.push('electricity');
+	}
+	if (document.getElementById('shower').checked) {
+		services.push('shower');
+	}
+	if (document.getElementById('heat').checked) {
+		services.push('heat');
+	}
+	if (document.getElementById('wifi').checked) {
+		services.push('wifi');
+	}
+	if (document.getElementById('laundry').checked) {
+		services.push('laundry');
+	}
+
+	host.services = services;
+
+	$.ajax({
+		type: "post",
+		url: "http://localhost:3000/host",
+		dataType: 'json',
+			data: JSON.stringify(host),
+			contentType: 'application/json',
+			success: function(data) {
+				// alert('success');
+				// alert(data);
+			}
+	});
 }
