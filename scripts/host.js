d0f4428d0a29100a16d01a31714918cc389ab917
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-11 17:40:34 -0500

    Checkboxes and submit button for host

--- scripts/host.js
+++ scripts/host.js
@@ -1,11 +1,11 @@
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	console.log('ID: ' + profile.getId());
-	console.log('Name: ' + profile.getName());
+	console.log('Name: ' + profile.getGivenName());
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());
 
-	insertProfileInfo(profile.getName(), profile.getImageUrl());
+	insertProfileInfo(profile.getGivenName(), profile.getImageUrl());
 }
 
 function signOut() {
@@ -20,4 +20,8 @@ function insertProfileInfo(name, picture) {
 	document.getElementById('profileImage').innerHTML = 
 		'<img src="' + picture + '" class="img-circle" alt="' + name + '">'
 	document.getElementById('name').innerHTML = name;
+}
+
+function submitHost() {
+	console.log('hi');
 }
