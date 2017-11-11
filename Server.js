0c1c83f798051c2298052814a38176c880602e8f
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-11 14:31:04 -0500

    Writing to JSON file

--- Server.js
+++ Server.js
@@ -3,6 +3,8 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+var fs = require("fs");
+
 router.use(function (req, res, next) {
 	console.log('/' + req.method);
 	next();
@@ -34,7 +36,7 @@ router.get('/', function(req, res) {
 
 app.use('/', router);
 
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/scripts'));
 
 // app.use('*', function(req, res) {
 // 	res.sendFile(path + '404.html');
@@ -42,4 +44,33 @@ app.use(express.static(__dirname + '/images'));
 
 app.listen(3000, function() {
 	console.log('Live at port 3000');
+});
+
+
+function writeFile(file) {
+	var user =  new Object();
+	user.type = "host";
+	user.name = "Candice Poon",
+	user.address = "2150 14th Street, Troy, NY 12180";
+	user.services = ["water", "food", "electricity"];
+	user.rating = [20, 8];
+	user = JSON.stringify(user);
+	if (file !== '') {
+		file = file.concat(',');
+	}
+	file = file.concat(user);
+	fs.writeFile('data/users.json', file, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
+}
+
+fs.readFile('data/users.json', 'utf8', function readFileCallback(err, data) {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		writeFile(data);
+	}
 });
