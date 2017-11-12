8d394243fae8ba1057083cf5c8c99af7438002b9
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-12 05:18:30 -0500

    Sorting and styling

--- scripts/host-list.js
+++ scripts/host-list.js
@@ -29,7 +29,7 @@ function populateList(data) {
 		    <div class="col-xs-8 "><h4 class="card-title">` + obj.name + `</h4>
 				 	<p class="card-text">` + servicesString + `
 				    </p>
-				    <button type="button" class="btn btn-success" 
+				    <button type="button" class="btn btn-info" 
 				    	onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=` + obj.email + `&su=Village Request',
 				    	 '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">Request</button>
 					<button type="button" class="btn btn-success" 
@@ -37,11 +37,11 @@ function populateList(data) {
 
 
 				    	<button onclick="increaseTU(` + obj.id + `)" type="button" class="btn btn-success" role="button" aria-disabled="true">
-                        <span class="glyphicon glyphicon-thumbs-up" id=""> ` + obj.rating[0] + `</span>
+                        <span class="glyphicon glyphicon-thumbs-up" id="thumbsUp"> ` + obj.rating[0] + `</span>
                     	</button>
 
                    		 <button onclick="increaseTD(` + obj.id + `)" type="button" class="btn btn-danger" role="button" aria-disabled="true">
-                        <span class="glyphicon glyphicon-thumbs-down" id=""> ` + obj.rating[1] + `</span>
+                        <span class="glyphicon glyphicon-thumbs-down" id="thumbsDown"> ` + obj.rating[1] + `</span>
                     	</button>
 				  </div>
 				</li>
@@ -61,25 +61,94 @@ function populateList(data) {
 		    <div class="col-xs-8"><h4 class="card-title">` + obj.name + `</h4>
 				 	<p class="card-text">` + servicesString + `
 				    </p>
-				    <button type="button" class="btn btn-success" 
+				    <button type="button" class="btn btn-info" 
 				    	onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=` + obj.email + `&su=Village Request',
 				    	 '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">Request</button>
 
 				    	 <button onclick="increaseTU(` + obj.id + `)" type="button" class="btn btn-success" role="button" aria-disabled="true">
-                        <span class="glyphicon glyphicon-thumbs-up" id=""> ` + obj.rating[0] + `</span>
+                        <span class="glyphicon glyphicon-thumbs-up" id="thumbsUp"> ` + obj.rating[0] + `</span>
                     	</button>
 
                    		 <button onclick="increaseTD(` + obj.id + `)" type="button" class="btn btn-danger" role="button" aria-disabled="true">
-                        <span class="glyphicon glyphicon-thumbs-down" id=""> ` + obj.rating[1] + `</span>
+                        <span class="glyphicon glyphicon-thumbs-down" id="thumbsDown"> ` + obj.rating[1] + `</span>
                     	</button>
 				  </div>
 				</li>
 			`;
 			}
 		}
-
-		
 	}
+}
+
+function sortAlpha() {
+	// function sortList() {
+	  var list, i, switching, b, shouldSwitch;
+	  list = document.getElementById("host-list");
+	  switching = true;
+	  /* Make a loop that will continue until
+	  no switching has been done: */
+	  while (switching) {
+	    // Start by saying: no switching is done:
+	    switching = false;
+	    b = list.getElementsByTagName("LI");
+	    // Loop through all list items:
+	    for (i = 0; i < (b.length - 1); i++) {
+	      // Start by saying there should be no switching:
+	      shouldSwitch = false;
+	      /* Check if the next item should
+	      switch place with the current item: */
+	      console.log(b[i].getElementsByTagName("H4")[0].innerHTML);
+	      if (b[i].getElementsByTagName("H4")[0].innerHTML.toLowerCase() > b[i+1].getElementsByTagName("H4")[0].innerHTML.toLowerCase()) {
+	        /* If next item is alphabetically lower than current item,
+	        mark as a switch and break the loop: */
+	        shouldSwitch= true;
+	        break;
+	      }
+	    }
+	    if (shouldSwitch) {
+	      /* If a switch has been marked, make the switch
+	      and mark the switch as done: */
+	      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+	      switching = true;
+	    }
+	  }
+	// }
+}
+
+function sortPop() {
+	// function sortList() {
+	  var list, i, switching, b, shouldSwitch;
+	  list = document.getElementById("host-list");
+	  switching = true;
+	  /* Make a loop that will continue until
+	  no switching has been done: */
+	  while (switching) {
+	    // Start by saying: no switching is done:
+	    switching = false;
+	    b = list.getElementsByTagName("LI");
+	    // Loop through all list items:
+	    for (i = 0; i < (b.length - 1); i++) {
+	      // Start by saying there should be no switching:
+	      shouldSwitch = false;
+	      /* Check if the next item should
+	      switch place with the current item: */
+	      var len = b[i].getElementsByTagName('BUTTON').length;
+	      var len1 = b[i+1].getElementsByTagName('BUTTON').length;
+	      if (Number(b[i].getElementsByTagName('BUTTON')[len-2].getElementsByTagName('span')[0].innerHTML) < Number(b[i+1].getElementsByTagName('BUTTON')[len1-2].getElementsByTagName('span')[0].innerHTML)) {
+	        /* If next item is alphabetically lower than current item,
+	        mark as a switch and break the loop: */
+	        shouldSwitch= true;
+	        break;
+	      }
+	    }
+	    if (shouldSwitch) {
+	      /* If a switch has been marked, make the switch
+	      and mark the switch as done: */
+	      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+	      switching = true;
+	    }
+	  }
+	// }
 }
 
 function increaseTU(id) {
