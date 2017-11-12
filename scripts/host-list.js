6dad38b84ee8a34f8dc88c0e2f79db9e1a3430a4
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-12 01:01:08 -0500

    Square cash functionality

--- scripts/host-list.js
+++ scripts/host-list.js
@@ -13,27 +13,48 @@ function populateList(data) {
 	var myArray = JSON.parse(data);
 	for (var i = 0; i < myArray.length; ++i) {
 		var obj = JSON.parse(myArray[i]);
-		var servicesString = '';
-		for (var j = 0; j < obj.services.length - 1; ++j) {
-			servicesString += obj.services[j] + ', ';
+		if (obj.type == "host") {
+			if (typeof obj.squareCash != 'undefined') {
+				var servicesString = '';
+				for (var j = 0; j < obj.services.length - 1; ++j) {
+					servicesString += obj.services[j] + ', ';
+				}
+				servicesString += obj.services[j];
+				document.getElementById('host-list').innerHTML += `
+				<li class="list-group-item">
+				  <div class="card-block profile_item">
+				  	<img src="` + obj.image + `" class="img-circle" alt="` + obj.name + `">
+				    <h4 class="card-title">` + obj.name + `</h4>
+				 	<p class="card-text">` + servicesString + `
+				    </p>
+				    <button type="button" class="btn btn-success" 
+				    	onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=` + obj.email + `&su=Village Request',
+				    	 '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">Request</button>
+					<button type="button" class="btn btn-success" 
+				    	onclick="window.open('` + obj.squareCash + `', '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">$</button>
+				  </div>
+				</li>
+			`;
+			} else {
+				var servicesString = '';
+				for (var j = 0; j < obj.services.length - 1; ++j) {
+					servicesString += obj.services[j] + ', ';
+				}
+				servicesString += obj.services[j];
+				document.getElementById('host-list').innerHTML += `
+				<li class="list-group-item">
+				  <div class="card-block profile_item">
+				  	<img src="` + obj.image + `" class="img-circle" alt="` + obj.name + `">
+				    <h4 class="card-title">` + obj.name + `</h4>
+				 	<p class="card-text">` + servicesString + `
+				    </p>
+				    <button type="button" class="btn btn-success" 
+				    	onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=` + obj.email + `&su=Village Request',
+				    	 '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">Request</button>
+				  </div>
+				</li>
+			`;
+			}
 		}
-		servicesString += obj.services[j];
-		document.getElementById('host-list').innerHTML += `
-		<li class="list-group-item">
-		  <div class="card-block profile_item">
-		  	<img src="` + obj.image + `" class="img-circle" alt="` + obj.name + `">
-		    <h4 class="card-title">` + obj.name + `</h4>
-		 	<p class="card-text">` + servicesString + `
-		    </p>
-		    <button type="button" class="btn btn-success" 
-		    	onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=` + obj.email + `&su=Village Request',
-		    	 '_blank', 'toolbar=yes, scrollbars=yes, resizable=yes, width=700, height=500');">Request</button>
-		  </div>
-		</li>
-	`;
 	}
-}
-
-function request() {
-
 }
