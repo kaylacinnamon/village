1c77a02e6b6fca26cb8eab4f085b67e08c115ae8
Kayla Cinnamon <cinnak@rpi.edu>
2017-11-11 23:31:59 -0500

    Fixed the other bug my bad

--- scripts/host-list.js
+++ scripts/host-list.js
@@ -13,19 +13,16 @@ function populateList(data) {
 	var myArray = JSON.parse(data);
 	for (var i = 0; i < myArray.length; ++i) {
 		var obj = JSON.parse(myArray[i]);
+		var servicesString = '';
+		for (var j = 0; j < obj.services.length - 1; ++j) {
+			servicesString += obj.services[j] + ', ';
+		}
+		servicesString += obj.services[j];
 		document.getElementById('host-list').innerHTML += `
 		<div class="card">
 		  <div class="card-block">
 		  	<img src="` + obj.image + `" class="img-circle" alt="` + obj.name + `">
-		    <h4 class="card-title">` + obj.name + `</h4>`;
-		    // <p class="card-text">`;
-		// console.log(obj.services);
-		var servicesString = '';
-		 for (var j = 0; j < obj.services.length - 1; ++j) {
-		 	servicesString += obj.services[j] + ', ';
-		 }
-		 servicesString += obj.services[j];
-		 document.getElementById('host-list').innerHTML += `
+		    <h4 class="card-title">` + obj.name + `</h4>
 		 	<p class="card-text">` + servicesString + `
 		    </p>
 		    <button type="button" class="btn btn-success" onclick="request()">Request</button>
